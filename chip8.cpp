acd28b6009185a556ffe57d985aef0d383eadbc5
Lucas <cassels@college.harvard.edu>
2017-10-19 21:17:32 -0400

    fixed formatting

--- chip8.cpp
+++ chip8.cpp
@@ -434,10 +434,10 @@ void Chip8::loadGame(string name){
 /* Keymapping:
 
 Keypad                 Keyboard
-1 2 3 C                1 2 3 4
-4 5 6 D                Q W E R
-7 8 9 E                A S D F
-A 0 B F                Z X C V
+1|2|3|C       -->      1|2|3|4
+4|5|6|D       -->      Q|W|E|R
+7|8|9|E       -->      A|S|D|F
+A|0|B|F       -->      Z|X|C|V
 
 keymap[x] = keypad[x] - so keymap[0xA] is the scancode for button A
 (scancodes: https://wiki.libsdl.org/SDL_Scancode) */
